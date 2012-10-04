754e148bacd40d88e3ee1a7f89631434619fd978
katowulf <katowulf@gmail.com>
2012-10-04 16:05:40 -0700

    switched from octocat to node-github

--- app.js
+++ app.js
@@ -1,17 +1,10 @@
 
-var Q      = require('q'),
-    _      = require('underscore'),
-    gh     = require('octonode'),
-    conf   = require('./config.js'),
-    client = gh.client(conf),
-    base64 = require('./base64.js');
-
-//var ghme   = client.me();
-//var ghuser = client.user('pksunkara');
-//var ghrepo = client.repo('pksunkara/hub');
-//var ghorg  = client.org('flatiron');
-//var ghgist = client.gist();
-//var ghteam = client.team(37);
+var Q       = require('q'),
+    _u       = require('underscore'),
+    Gh      = require('node-github'),
+    conf    = require('./config.js'),
+    base64  = require('./base64.js'),
+    email   = require("emailjs");
 
 function fileStats(stats, filePath, repo) {
    var def = Q.defer();
@@ -22,7 +15,8 @@ function fileStats(stats, filePath, repo) {
          def.reject(err);
       }
       else {
-         console.log('    > file: ', filePath.substr(Math.max(filePath.lastIndexOf('/'), 0)));
+
+         console.log('    > file: ', filePath);
          var decodedContent = base64.decode(contents.content);
          stats.lines += decodedContent.split("\n").length;
          stats.files++;
@@ -65,23 +59,17 @@ function dirStats(stats, path, repo) {
    return def.promise;
 }
 
-function readCommits(stats, repo) {
-   var def = Q.defer();
-   repo.commits(function(err, commits) {
-      if( err ) {
-         def.reject(err);
+function readCommits(stats) {
+   return _accumulate('repos', 'getCommits', {repo: stats.name, user: stats.fullName.substr(0, stats.fullName.indexOf('/')), per_page: 2}, true).then(
+      function(count) {
+         console.log('the count', count);
+         stats.commits = count;
       }
-      else {
-         stats.commits = commits.length;
-         def.resolve();
-      }
-   });
-   return def.promise;
+   );
 }
 
 function readRepo(stats, repoData) {
-   console.log('REPO: ', repoData.name);
-   var repo = client.repo(repoData.full_name), repoStats = stats[ repoData.name ] = {
+   var repoStats = stats[ repoData.name ] = {
       name: repoData.name,
       fullName: repoData.full_name,
       size: repoData.size,
@@ -95,12 +83,12 @@ function readRepo(stats, repoData) {
       commits: 0,
       bytes: 0
    };
-   return Q.all([ readCommits(repoStats, repo), dirStats(repoStats, '', repo) ]);
+   return Q.all([ readCommits(repoStats)/*, dirStats(repoStats)*/ ]);
 }
 
 function accumulateRepos(stats, repoList) {
    if( conf.repoFilter ) {
-      repoList = _.filter(repoList, conf.repoFilter);
+      repoList = _u.filter(repoList, conf.repoFilter);
    }
    var promises = [], i = repoList.length;
    while(i--) {
@@ -110,19 +98,11 @@ function accumulateRepos(stats, repoList) {
 }
 
 function processOrg(stats, org) {
-   var deferred = Q.defer();
-   console.log("------------\nORG: "+ org.login + "\n------------");
-   client.org(org.login).repos(function(err, repoList) {
-      if( err ) {
-         deferred.reject(err);
-      }
-      else {
-         accumulateRepos(stats, repoList)
-            .then(deferred.resolve)
-            .fail(deferred.reject);
-      }
+   var opts = {org: org.login, per_page: 2};
+   return _accumulate('repos', 'getFromOrg', opts).then(function(repoList) {
+      console.log('processOrg', org, repoList);
+      return accumulateRepos(stats, repoList);
    });
-   return deferred.promise;
 }
 
 function processOrgs(stats, orgList) {
@@ -133,24 +113,64 @@ function processOrgs(stats, orgList) {
    return Q.all(promises);
 }
 
-var user = client.me(), stats = {};
+function sendEmail(subject, message) {
+   if( conf.email && conf.smtp ) {
+      // send the message and get a callback with an error or details of the message that was sent
+      var server  = email.server.connect(conf.smtp);
+      server.send({
+         text:    message,
+         from:    conf.email,
+         to:      conf.email,
+         subject: subject
+      }, function(err, message) {
+         if( err ) {
+            console.error(err);
+         }
+         else {
+            console.log("Email Delivered");
+         }
+         server.smtp.close();
+      });
+   }
+}
 
-Q.all(
-      Q.ninvoke(user, 'repos')
+function _accumulate(method, fxName, opts, countOnly, masterList) {
+   console.log('accumulate', method, fxName, masterList && masterList.length);
+   var count = 0, max = opts.per_page? opts.per_page : 30;
+   masterList || (masterList = []);
+   return Q.ninvoke(new Gh({version: '3.0.0'})[method], fxName, opts).then(function(list) {
+      if( countOnly ) { count += list.length; }
+      else { masterList = masterList.concat(list); }
+
+      if( list && list.length == max ) {
+         opts.page = opts.page? opts.page + 1 : 2;
+         return _accumulate(method, fxName, opts, countOnly, masterList).then(function(n) {
+            countOnly && (count += n);
+         });
+      }
+      else {
+         return Q.when(countOnly? count : masterList);
+      }
+   });
+}
+
+var stats = {};
+
+Q.all([
+      Q.ninvoke(new Gh({version: '3.0.0'}).repos, 'getFromUser', {user: conf.user})
          .then(function(list) {
-            console.log("------------\nUSER: "+ conf.username +"\n------------");
             return accumulateRepos(stats, list);
          }),
-      Q.ninvoke(user, 'orgs')
+      Q.ninvoke(new Gh({version: '3.0.0'}).orgs, 'getFromUser', {user: conf.user})
          .then(function(list) {
             return processOrgs(stats, list);
          })
-   )
+   ])
    .then(
-      function() { console.log('stats', stats); }
+      function() { console.log(stats); }
    )
    .fail(function(e) {
-//      console.error(e.toString());
+      sendEmail('unable to generate stats', e.stack);
       console.error(e.stack);
    });
 
--- config.sample.js
+++ config.sample.js
@@ -1,16 +1,32 @@
 
 module.exports = {
-   username: 'github_user',
-   password: 'github_pass',
-   email:    'notified on errors',
+   // required: the account we're going to collect stats for
+   user: 'github_username',
+
+   // uncomment and configure to get an email any time an error occurs
+   //email:    'your@email.com',
+
+   // if `email` is uncommented, this is the outgoing smtp server used
+   smtp: {
+      user:    "smtp_login (probably your email address)",
+      password:"smtp_password (probably your email password)",
+      host:    "smtp.gmail.com",
+      ssl:     true
+   },
+
+   // comment this out (or `return true`) to generate stats for all repos
    repoFilter: function(repo) {
       return !repo.fork && repo.name.match(/^[._~-]/);
    },
+
+   // comment this out (or `return true`) to generate stats for all directories
    dirFilter: function(dir) {
-      return !dir.name.match(/^[._~-]/) && !(dir.name in {lib: 1, libs: 1});
+      return !dir.name.match(/^[._]/) && !(dir.name in {lib: 1, libs: 1, node_modules: 1}); // ignore things like .ssh
    },
+
+   // comment this out (or `return true`) to generate stats for all files
    fileFilter: function(file) {
-      return !file.name.match(/^[._~-]/);
+      return !file.name.match(/^[._]/) && !file.name.match(/[~-]$/); // ignore things like .gitignore and .gitattributes, and backup.txt~
    }
 };
 
