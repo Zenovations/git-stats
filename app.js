1588b56d44981d6cac9c156d5933374014a7ae95
katowulf <katowulf@gmail.com>
2012-10-05 17:10:00 -0700

    stats now compile completely working on output formats and destinations

--- app.js
+++ app.js
@@ -1,21 +1,48 @@
 
 var Q       = require('q'),
-    _u       = require('underscore'),
-    Gh      = require('node-github'),
+    _       = require('underscore'),
+    Gh      = require('github'),
     conf    = require('./config.js'),
     base64  = require('./base64.js'),
-    email   = require("emailjs");
+    email   = require("emailjs"),
+    debug   = conf.debug;
 
-function fileStats(stats, filePath, repo) {
+var nodemailer = require("nodemailer");
+var github = new Gh({version: '3.0.0'});
+var RE = /^((?>[a-zA-Z\d!#$%&'*+\-/=?^_`{|}~]+\x20*|"((?=[\x01-\x7f])[^"\\]|\\[\x01-\x7f])*"\x20*)*(?<angle><))?((?!\.)(?>\.?[a-zA-Z\d!#$%&'*+\-/=?^_`{|}~]+)+|"((?=[\x01-\x7f])[^"\\]|\\[\x01-\x7f])*")@(((?!-)[a-zA-Z\d\-]+(?<!-)\.)+[a-zA-Z]{2,}|\[(((?(?<!\[)\.)(25[0-5]|2[0-4]\d|[01]?\d?\d)){4}|[a-zA-Z\d\-]*[a-zA-Z\d]:((?=[\x01-\x7f])[^\\\[\]]|\\[\x01-\x7f])+)\])(?(angle)>)$/;
+
+/**
+ * Override console output for simple debugging
+ */
+var _console = console;
+var console = {
+   log: function() {
+      debug && _console.log.apply(_console, _.toArray(arguments));
+   },
+   warn: function() {
+      debug && _console.warn.apply(_console, _.toArray(arguments));
+   },
+   error: function() {
+      debug && _console.error.apply(_console, _.toArray(arguments));
+   }
+};
+
+function fileStats(stats, filePath) {
    var def = Q.defer();
-   repo.contents(filePath, function(err, contents) {
+
+   var opts = {
+      user: stats.owner,
+      repo: stats.name,
+      path: filePath
+   };
+
+   github.repos.getContent(opts, function(err, contents) {
       if( err ) {
          console.error('ERROR (fileStats)', err);
          console.log(err);
          def.reject(err);
       }
       else {
-
          console.log('    > file: ', filePath);
          var decodedContent = base64.decode(contents.content);
          stats.lines += decodedContent.split("\n").length;
@@ -27,48 +54,59 @@ function fileStats(stats, filePath, repo) {
    return def.promise;
 }
 
-function dirStats(stats, path, repo) {
+function dirStats(stats, path) {
    var def = Q.defer();
-   console.log(' + dir: ', stats.fullName, path);
-   if( !repo ) { repo = client.repo(stats.fullName); }
 
-   repo.contents(path, function(err, files) {
-      if( err ) {
-         console.error('ERROR (dirStats)', stats.fullName, path);
-         def.reject(err);
-      }
-      else {
-         var promises = [], i = -1, len = files.length, filePath, f;
-         while(++i < len) {
-            f = files[i];
-            filePath = f.path;
-            if( f.type == 'dir' && (!conf.dirFilter || conf.dirFilter(f)) ) {
-               promises.push(dirStats(stats, filePath, repo));
-            }
-            else if( f.type == 'file' && (!conf.fileFilter || conf.fileFilter(f)) ) {
-               promises.push(fileStats(stats, filePath, repo));
-            }
+   if( conf.deep ) {
+      console.log(' + dir: ', stats.fullName, path);
+
+      var opts = {
+         user: stats.owner,
+         repo: stats.name,
+         path: path
+      };
+
+      github.repos.getContent(opts, function(err, files) {
+         if( err ) {
+            console.error('ERROR (dirStats)', stats.fullName, path);
+            def.reject(err);
          }
-         Q.all(promises).then(function() {
-            console.log('all dirStats promises resolved', path);
-            def.resolve();
-         })
-      }
-   });
+         else {
+            var promises = [], i = -1, len = files.length, filePath, f;
+            while(++i < len) {
+               f = files[i];
+               filePath = f.path;
+               if( f.type == 'dir' && (!conf.dirFilter || conf.dirFilter(f)) ) {
+                  promises.push(dirStats(stats, filePath));
+               }
+               else if( f.type == 'file' && (!conf.fileFilter || conf.fileFilter(f)) ) {
+                  promises.push(fileStats(stats, filePath));
+               }
+            }
+            Q.all(promises).then(function() {
+               def.resolve();
+            })
+         }
+      });
+   }
+   else {
+      def.resolve();
+   }
 
    return def.promise;
 }
 
 function readCommits(stats) {
-   return _accumulate('repos', 'getCommits', {repo: stats.name, user: stats.fullName.substr(0, stats.fullName.indexOf('/')), per_page: 2}, true).then(
-      function(count) {
-         console.log('the count', count);
-         stats.commits = count;
-      }
-   );
+   var opts = {repo: stats.name, user: stats.fullName.substr(0, stats.fullName.indexOf('/')), per_page: 100};
+   return _accumulate('repos', 'getCommits', opts, true)
+      .then(function(n) {
+         stats.commits += n;
+         return true;
+      });
 }
 
 function readRepo(stats, repoData) {
+   console.log('reading repo', repoData.full_name);
    var repoStats = stats[ repoData.name ] = {
       name: repoData.name,
       fullName: repoData.full_name,
@@ -78,17 +116,21 @@ function readRepo(stats, repoData) {
       description: repoData.description,
       homepage: repoData.homepage,
       url: repoData.html_url,
+      owner: repoData.owner.login || repoData.owner.name,
+      watchers: repoData.watchers,
+      issues: repoData.open_issues,
+      forks: repoData.forks,
       files: 0,
       lines: 0,
       commits: 0,
       bytes: 0
    };
-   return Q.all([ readCommits(repoStats)/*, dirStats(repoStats)*/ ]);
+   return Q.all([ readCommits(repoStats), dirStats(repoStats, '') ]);
 }
 
-function accumulateRepos(stats, repoList) {
+function processRepos(stats, repoList) {
    if( conf.repoFilter ) {
-      repoList = _u.filter(repoList, conf.repoFilter);
+      repoList = _.filter(repoList, conf.repoFilter);
    }
    var promises = [], i = repoList.length;
    while(i--) {
@@ -98,10 +140,10 @@ function accumulateRepos(stats, repoList) {
 }
 
 function processOrg(stats, org) {
-   var opts = {org: org.login, per_page: 2};
+   console.log('processOrg', org.login);
+   var opts = {org: org.login};
    return _accumulate('repos', 'getFromOrg', opts).then(function(repoList) {
-      console.log('processOrg', org, repoList);
-      return accumulateRepos(stats, repoList);
+      return processRepos(stats, repoList);
    });
 }
 
@@ -113,43 +155,41 @@ function processOrgs(stats, orgList) {
    return Q.all(promises);
 }
 
-function sendEmail(subject, message) {
-   if( conf.email && conf.smtp ) {
-      // send the message and get a callback with an error or details of the message that was sent
-      var server  = email.server.connect(conf.smtp);
-      server.send({
-         text:    message,
-         from:    conf.email,
-         to:      conf.email,
-         subject: subject
-      }, function(err, message) {
-         if( err ) {
-            console.error(err);
-         }
-         else {
-            console.log("Email Delivered");
-         }
-         server.smtp.close();
-      });
-   }
+function sendEmail(to, message) {
+   var p = conf.email.protocol;
+   var smtpTransport = nodemailer.createTransport(p, conf.email[p]);
+
+   // setup e-mail data with unicode symbols
+   var mailOptions = _.extend({to: to}, conf.email.sendOptions, (typeof(message)==='string'? {text: message} : message));
+
+   // send mail with defined transport object
+   smtpTransport.sendMail(mailOptions, function(error, response){
+      if(error){
+         console.log(error);
+      }else{
+         console.log("Message sent: " + response.message);
+      }
+
+      //if you don't want to use this transport object anymore, uncomment following line
+      smtpTransport.close(); // shut down the connection pool, no more messages
+   });
 }
 
-function _accumulate(method, fxName, opts, countOnly, masterList) {
-   console.log('accumulate', method, fxName, masterList && masterList.length);
-   var count = 0, max = opts.per_page? opts.per_page : 30;
-   masterList || (masterList = []);
-   return Q.ninvoke(new Gh({version: '3.0.0'})[method], fxName, opts).then(function(list) {
-      if( countOnly ) { count += list.length; }
-      else { masterList = masterList.concat(list); }
-
-      if( list && list.length == max ) {
+function _accumulate(apiGroup, fxName, opts, countOnly, accumulatedData) {
+   opts = _.extend({}, {per_page: 100}, opts);
+   countOnly || (countOnly = false);
+   accumulatedData || (accumulatedData = countOnly? 0 : []);
+//   console.log('accumulating', apiGroup+'.'+fxName, opts);
+   return Q.ninvoke(github[apiGroup], fxName, opts).then(function(list) {
+      if( countOnly ) { accumulatedData += list.length; }
+      else if( list ) { accumulatedData = accumulatedData.concat(list); }
+      if( list && list.length == opts.per_page ) {
          opts.page = opts.page? opts.page + 1 : 2;
-         return _accumulate(method, fxName, opts, countOnly, masterList).then(function(n) {
-            countOnly && (count += n);
-         });
+         return _accumulate(apiGroup, fxName, opts, countOnly, accumulatedData);
       }
       else {
-         return Q.when(countOnly? count : masterList);
+//         console.log('accumulated '+(typeof(accumulatedData)==='number'? accumulatedData : accumulatedData.length));
+         return accumulatedData;
       }
    });
 }
@@ -157,20 +197,55 @@ function _accumulate(method, fxName, opts, countOnly, masterList) {
 var stats = {};
 
 Q.all([
-      Q.ninvoke(new Gh({version: '3.0.0'}).repos, 'getFromUser', {user: conf.user})
+      Q.ninvoke(github.repos, 'getFromUser', {user: conf.user}) //todo use accumulate
          .then(function(list) {
-            return accumulateRepos(stats, list);
+            return processRepos(stats, list);
          }),
-      Q.ninvoke(new Gh({version: '3.0.0'}).orgs, 'getFromUser', {user: conf.user})
+      Q.ninvoke(github.orgs, 'getFromUser', {user: conf.user}) //todo use accumulate
          .then(function(list) {
             return processOrgs(stats, list);
          })
    ])
-   .then(
-      function() { console.log(stats); }
-   )
+   .then(function() {
+      var out;
+      switch(conf.format) {
+         case 'json':
+            out = conf.compress? JSON.stringify(stats) : JSON.stringify(stats, null, 2);
+            break;
+         case 'xml':
+            var jsonxml = require('jsontoxml');
+            out = jsonxml({stats: stats}, {escape: true, xmlHeader: true});
+            conf.compress || (out = require('pretty-data').pd.xml(out));
+            break;
+         case 'csv':
+            var json2csv = require('json2csv'), data = _.toArray(stats);
+            out = json2csv.parse({
+               data: data,
+               fields: data.length? _.keys(data[0]) : []
+            });
+            break;
+         default:
+            throw new Error('invalid output format: '+conf.format);
+      }
+
+      if( conf.to === 'stdout' ) {
+         process.stdout.write(out+"\n");
+      }
+      else if( RE.test(conf.to) ) {
+         sendEmail(conf.to, {text: 'see attached', attachments: [
+            { fileName: 'stats.'+conf.format, contents: out }
+         ]});
+         console.log('email delivered', conf.to);
+      }
+      else {
+         //todo
+         throw new Error('file output format not implemented yet');
+      }
+   })
    .fail(function(e) {
-      sendEmail('unable to generate stats', e.stack);
+      if( conf.send_errors_to ) {
+         sendEmail(conf.send_errors_to, e.stack);
+      }
       console.error(e.stack);
    });
 
--- config.sample.js
+++ config.sample.js
@@ -3,16 +3,19 @@ module.exports = {
    // required: the account we're going to collect stats for
    user: 'github_username',
 
-   // uncomment and configure to get an email any time an error occurs
-   //email:    'your@email.com',
+   // collect statistics such as how many lines of code, how many files, et al
+   // this takes significantly more requests and cpu cycles, so if this information
+   // isn't going to be used, set this to false
+   deep: true,
 
-   // if `email` is uncommented, this is the outgoing smtp server used
-   smtp: {
-      user:    "smtp_login (probably your email address)",
-      password:"smtp_password (probably your email password)",
-      host:    "smtp.gmail.com",
-      ssl:     true
-   },
+   // how shall we output the data?
+   format: 'json', // 'json', 'xml', or 'csv'
+
+   // where shall we output it to?
+   to: 'stdout',   // 'stdout', a writable file path, or 'email' (be sure to configure settings below)
+
+   // setting this to false will add formatting (returns, indentation) to the outputted data
+   compress: true,
 
    // comment this out (or `return true`) to generate stats for all repos
    repoFilter: function(repo) {
@@ -21,12 +24,46 @@ module.exports = {
 
    // comment this out (or `return true`) to generate stats for all directories
    dirFilter: function(dir) {
-      return !dir.name.match(/^[._]/) && !(dir.name in {lib: 1, libs: 1, node_modules: 1}); // ignore things like .ssh
+      return !dir.name.match(/^[._]/) && !(dir.name in {lib: 1, libs: 1, node_modules: 1}); // ignore things like .ssh, lib/, and node_modules (included libs)
    },
 
    // comment this out (or `return true`) to generate stats for all files
    fileFilter: function(file) {
       return !file.name.match(/^[._]/) && !file.name.match(/[~-]$/); // ignore things like .gitignore and .gitattributes, and backup.txt~
-   }
+   },
+
+   // true to send email on any error (be sure to configure settings below)
+   report_errors: true,
+
+   // configure if `report_errors` is true or if `to` is email
+   email: {
+      // 'smtp', or 'sendmail'
+      protocol: 'smtp',
+
+      sendOptions: {
+         from: "Sender Name <sender@example.com>", // sender address
+         to: "receiver1@example.com, receiver2@example.com", // list of receivers
+         subject: "[git-stats] stats failed"
+      },
+
+      smtp: {
+         host: "smtp.gmail.com", // hostname
+         secureConnection: true, // use SSL
+         port: 465, // port for secure SMTP
+         auth: {
+            user: "gmail.user@gmail.com",
+            pass: "userpass"
+         }
+      },
+
+      sendmail: {
+         path: "/usr/local/bin/sendmail",
+         args: ["-f sender@example.com"]
+      }
+   },
+
+   // setting this to true generates lots of logging to stdout
+   debug: false
+
 };
 
